0ae8a532a96debe6f50fc35c8a6fbaf44dcd5c79
GoodMan <469603398@qq.com>
2018-01-20 14:58:33 +0800

    提交 通用mapper

---
+++ jesses-manage/jesses-manage-mapper/src/main/java/com/jesses/manage/mapper/ItemCatMapper.java
@@ -0,0 +1,8 @@
+package com.jesses.manage.mapper;
+
+import com.github.abel533.mapper.Mapper;
+import com.jesses.manage.pojo.ItemCat;
+
+
+public interface ItemCatMapper extends Mapper<ItemCat> {
+}
